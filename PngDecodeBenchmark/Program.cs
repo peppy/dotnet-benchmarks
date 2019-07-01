433b2173f22407f235446e4ac6713161177c2d9b
Dean Herbert <pe@ppy.sh>
2019-07-01 12:25:18 +0900

    Initial commit

---
+++ PngDecodeBenchmark/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Advanced;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.Memory;
+
+namespace BenchmarkSandbox
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<SimpleBenchmark>();
+        }
+    }
+
+    [MemoryDiagnoser]
+    public class SimpleBenchmark
+    {
+        public SimpleBenchmark()
+        {
+            // generate raw output
+            using (var stream = File.OpenRead("Baseline.png"))
+            using (var image = Image.Load<Rgba32>(stream))
+            {
+                File.WriteAllBytes("Raw", MemoryMarshal.Cast<Rgba32,byte>(image.GetPixelSpan()).ToArray());
+            }
+
+            using (var stream = File.OpenRead("Pixelmator-trim.png"))
+            using (var image = Image.Load<Rgba32>(stream))
+            {
+                File.WriteAllBytes("Raw-trim", MemoryMarshal.Cast<Rgba32,byte>(image.GetPixelSpan()).ToArray());
+            }
+        }
+
+        [Benchmark]
+        public void Baseline()
+        {
+            using (var stream = File.OpenRead("Baseline.png"))
+            using (var image = Image.Load<Rgba32>(stream))
+            {
+                image.GetPixelSpan();
+            }
+        }
+
+        [Benchmark]
+        public void Imagemagick()
+        {
+            using (var stream = File.OpenRead("ImageMagick.png"))
+            using (var image = Image.Load<Rgba32>(stream))
+            {
+                image.GetPixelSpan();
+            }
+        }
+
+        [Benchmark]
+        public void ImageOptim()
+        {
+            using (var stream = File.OpenRead("ImageOptim.png"))
+            using (var image = Image.Load<Rgba32>(stream))
+            {
+                image.GetPixelSpan();
+            }
+        }
+
+        [Benchmark]
+        public void TrimmedPixelmator()
+        {
+            using (var stream = File.OpenRead("Pixelmator-trim.png"))
+            using (var image = Image.Load<Rgba32>(stream))
+            {
+                image.GetPixelSpan();
+            }
+        }
+
+        [Benchmark]
+        public void TrimmedImagemagick()
+        {
+            using (var stream = File.OpenRead("ImageMagick-trim.png"))
+            using (var image = Image.Load<Rgba32>(stream))
+            {
+                image.GetPixelSpan();
+            }
+        }
+
+        [Benchmark]
+        public void TrimmedImageOptim()
+        {
+            using (var stream = File.OpenRead("ImageOptim-trim.png"))
+            using (var image = Image.Load<Rgba32>(stream))
+            {
+                image.GetPixelSpan();
+            }
+        }
+
+        [Benchmark]
+        public void Raw()
+        {
+            using (var memory = Configuration.Default.MemoryAllocator.Allocate<Rgba32>(1024 * 1024))
+            using (var stream = File.OpenRead("Raw"))
+            {
+                stream.Read(MemoryMarshal.Cast<Rgba32, byte>(memory.Memory.Span));
+
+                using (var image = Image.WrapMemory(memory, 1024, 1024))
+                {
+                    image.GetPixelSpan();
+                }
+            }
+        }
+
+        [Benchmark]
+        public void TrimmedRaw()
+        {
+            using (var memory = Configuration.Default.MemoryAllocator.Allocate<Rgba32>(1010 * 62))
+            using (var stream = File.OpenRead("Raw-trim"))
+            {
+                stream.Read(MemoryMarshal.Cast<Rgba32, byte>(memory.Memory.Span));
+
+                using (var image = Image.WrapMemory(memory, 1010, 62))
+                {
+                    image.GetPixelSpan();
+                }
+            }
+        }
+    }
+}
---
+++ PngDecodeBenchmark/README.md
@@ -0,0 +1,26 @@
+```
+dotnet run -c Release
+```
+
+
+```ini
+BenchmarkDotNet=v0.11.5, OS=macOS Mojave 10.14.5 (18F132) [Darwin 18.6.0]
+Intel Xeon CPU E5-1620 v2 3.70GHz, 1 CPU, 8 logical and 4 physical cores
+.NET Core SDK=2.2.300
+  [Host]     : .NET Core 2.2.5 (CoreCLR 4.6.27617.05, CoreFX 4.6.27618.01), 64bit RyuJIT
+  DefaultJob : .NET Core 2.2.5 (CoreCLR 4.6.27617.05, CoreFX 4.6.27618.01), 64bit RyuJIT
+```
+
+|             Method  |        Mean |      Error |     StdDev |  Width | Height | Size  |
+|-------------------- |------------:|-----------:|-----------:|-------:|-------:|------:|
+|          ImageOptim | 12,600.3 us |  48.633 us |  40.611 us | 1024px | 1024px |   9kb |
+| ImageMagick convert |  6,669.7 us | 119.754 us | 147.069 us | 1024px | 1024px |  14kb |
+|            Baseline |  3,416.0 us |  68.152 us | 106.105 us | 1024px | 1024px |  37kb |
+|  Trimmed ImageOptim |    899.2 us |   2.917 us |   2.436 us | 1010px |   62px |   9kb |
+|  Trimmed Pixelmator |    841.2 us |   4.108 us |   3.642 us | 1010px |   62px |  18kb |
+| Trimmed Imagemagick |    664.0 us |   5.053 us |   4.727 us | 1010px |   62px |  11kb |
+|                 Raw |    497.1 us |   9.460 us |   8.849 us | 1024px | 1024px |4194kb |
+|          TrimmedRaw |     71.7 us |   1.525 us |   1.983 us | 1010px |   62px | 250kb |
+
+- Baseline is direct output from bmfont generator (https://www.angelcode.com/products/bmfont/)
+- Raw is raw RGBA32 pixel stream
